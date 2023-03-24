6b5620c6c07a4740d4c2915faccdc8e351f0883b
StylezZz <a20202692@pucp.edu.pe>
2023-03-24 15:08:04 -0500

    pruebas en js y html

---
+++ semana4/dia4/pruebas.js
@@ -0,0 +1,17 @@
+let nombre = "Juan";
+
+let productos = ["guillermo", "juan", "pedro", 1000, true];
+
+console.log(nombre)
+console.log(productos)
+
+const pi = 3.141516;
+
+function suma(a, b) {
+  return a + b;
+}
+
+function mandarSaludo(){
+    console.log("Hola Mundo");
+    alert("Hola Mundo");
+}
